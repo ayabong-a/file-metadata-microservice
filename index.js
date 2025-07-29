fe8a58c6c5ba74986d626e7627ca10d0bbc8d463
Athianda Bukula <mtsoloayabonga@gmail.com>
2025-07-29 15:15:22 +0200

    setup route to analyse form file

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express());
+app.use(express.static("public"));
+app.use(cors());
 
-app.use(express.static('public'));
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get('/api', (req, res) => {
-    res.json({message: "api running"})
-});
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const file = req.file;
 
+  if (!file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
+  res.json({ name: file.originalname, type: file.mimetype, size: file.size });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-    console.log(`listening on port ${listener.address().port}`)
+  console.log(`listening on port ${listener.address().port}`);
 });
